2ec7215d81dca7f921deca3f6d80fb42d1a1a883
Hachimi Bouizegarene <hachimi.bouizegarene@outlook.fr>
2024-02-03 00:54:37 +0100

    fix bug await

--- src/server.js
+++ src/server.js
@@ -7,9 +7,6 @@ import { getData } from './api.js';
 hbs.registerPartial('header', fs.readFileSync("src/templates/header.hbs", "utf-8"));
 hbs.registerPartial('footer', fs.readFileSync("src/templates/footer.hbs", "utf-8"));
 
-const data = await getData("https://gateway.marvel.com:443/v1/public/characters?limit=100")
-
-
 
 const fastify = Fastify({
     logger: true
@@ -21,17 +18,25 @@ fastify.register(view, {
     }
 })
 
-fastify.get("/", (req, reply) => {
-    console.log(data)
-    reply.view("src/templates/index.hbs", { characters: data });
-});
 
 // Run the server!
-try {
-    await fastify.listen({ port: 3000 , host:"0.0.0.0"} , (err, address)=>{
-        console.log(address)
-    })
-} catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+async function runServer(){
+    const data = await getData("https://gateway.marvel.com:443/v1/public/characters?limit=100")
+
+    fastify.get("/", (req, reply) => {
+        console.log(data)
+        reply.view("src/templates/index.hbs", { characters: data });
+    });
+    
+
+    try {
+        fastify.listen({ port: 3000 , host:"0.0.0.0"} , (err, address)=>{
+            console.log(address)
+        })
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
 }
+
+runServer()
